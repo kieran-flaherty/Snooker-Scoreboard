3296de97e587a8d97f58aa984a894a5da092cfba
Kieran Flaherty <kieranflaherty1995@gmail.com>
2020-05-03 21:34:40 +0100

    refactor: pull out BreakText function component in player.jsx

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Player = (props) => {
-  const BreakText = (props) => {
-    return props.atTable ? <h5>Break: {props.break}</h5> : <h5>-</h5>;
-  };
+const BreakText = (props) => {
+  return props.atTable ? <h5>Break: {props.break}</h5> : <h5>-</h5>;
+};
 
+const Player = (props) => {
   const classText = `m-1 text-center w-50 ${
     props.atTable ? "border rounded current-player" : ""
   }`;
